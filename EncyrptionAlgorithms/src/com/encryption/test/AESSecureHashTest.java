bcfd25a8eb3d29fcf3c277c1be3776d87bd2cd44
Khaja.Shaik <Khaja.Shaik@cashe.co.in>
2020-08-29 15:09:25 +0530

    Check-in for Encryption Algorithms.

---
+++ EncyrptionAlgorithms/src/com/encryption/test/AESSecureHashTest.java
@@ -0,0 +1,40 @@
+package com.encryption.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.encryption.AES256SecureHash;
+import com.encryption.AESSecureHash;
+
+public class AESSecureHashTest {
+
+	@Test public void testAESEncryptionDecryption() {
+
+		String inputValue = "AXBHi987654KJIO9087";
+
+		System.out.println("Input value:"+inputValue);
+
+		String encryptedValue = AESSecureHash.encrypt(inputValue);
+		System.out.println("Encrypted value:"+encryptedValue);
+
+		String decryptedValue =AESSecureHash.decrypt(encryptedValue);
+		System.out.println("Decrypted value:"+decryptedValue);		
+
+		Assert.assertTrue("Encrypted and Decrypted values for AES, must be exactly same", inputValue.equals(decryptedValue));
+	}
+
+	@Test public void testAES256EncryptionDecryption() {
+
+		String inputValue = "{0(&UI15998~>/345YUNMBHJIOPER1234";
+
+		System.out.println("Input value:"+inputValue);
+
+		String encryptedValue = AES256SecureHash.encrypt(inputValue);
+		System.out.println("Encrypted value:"+encryptedValue);
+
+		String decryptedValue = AES256SecureHash.decrypt(encryptedValue) ;
+		System.out.println("Decrypted value:"+decryptedValue);
+		
+		Assert.assertTrue("Encrypted and Decrypted values for AES256, must be exactly same", inputValue.equals(decryptedValue));
+	}
+}
