6c8ccd9e9af07444176cf74f432afc6fe5324af2
Khaja.Shaik <Khaja.Shaik@cashe.co.in>
2020-08-30 14:16:27 +0530

    Implemented basic version of RSA algorithm.

---
+++ EncyrptionAlgorithms/src/com/encryption/asymmetric/RSASecureHash.java
@@ -0,0 +1,48 @@
+package com.encryption.asymmetric;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+public class RSASecureHash {
+
+	static String plainText = "Plain text which need to be encrypted by Java RSA Encryption in ECB Mode";
+
+	public static byte[] encrypt (String plainText,PublicKey publicKey ) throws Exception
+	{
+		//Get Cipher Instance RSA With ECB Mode and OAEPWITHSHA-512ANDMGF1PADDING Padding
+		Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-512ANDMGF1PADDING");
+
+		//Initialize Cipher for ENCRYPT_MODE
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+		//Perform Encryption
+		return cipher.doFinal(plainText.getBytes());
+	}
+
+	public static String decrypt (byte[] cipherTextArray, PrivateKey privateKey) throws Exception
+	{
+		//Get Cipher Instance RSA With ECB Mode and OAEPWITHSHA-512ANDMGF1PADDING Padding
+		Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-512ANDMGF1PADDING");
+
+		//Initialize Cipher for DECRYPT_MODE
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+		//Perform Decryption
+		byte[] decryptedTextArray = cipher.doFinal(cipherTextArray);
+
+		return new String(decryptedTextArray);
+	}
+
+	public static KeyPair getGeneratedKeyPair() throws NoSuchAlgorithmException {
+		
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		keyPairGenerator.initialize(4096);
+
+		// Generate the KeyPair
+		return keyPairGenerator.generateKeyPair();
+	}
+}
---
+++ EncyrptionAlgorithms/src/com/encryption/asymmetric/test/RSASecureHashText.java
@@ -0,0 +1,34 @@
+package com.encryption.asymmetric.test;
+
+import java.security.KeyPair;
+import java.util.Base64;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.encryption.asymmetric.RSASecureHash;
+public class RSASecureHashText {
+
+    private static String ORIGINAL_TEXT = "Plain text which need to be encrypted by Java RSA Encryption in ECB Mode";
+    
+	@Test public void testRSAEncryptionDecryption() throws Exception {
+		
+        // Get an instance of the RSA key generator
+    	KeyPair keyPair = RSASecureHash.getGeneratedKeyPair();
+        
+        // Get the public and private key
+        System.out.println("Original Text : "+ORIGINAL_TEXT);
+        
+        // Encryption.
+        byte[] cipherText = RSASecureHash.encrypt(ORIGINAL_TEXT, keyPair.getPublic());
+        String encryptedText = Base64.getEncoder().encodeToString(cipherText);
+        System.out.println("Encrypted Text: "+encryptedText);
+        
+        // Decryption.
+        String decryptedText = RSASecureHash.decrypt(cipherText, keyPair.getPrivate());
+        System.out.println("DeCrypted Text: "+decryptedText);
+        
+		Assert.assertTrue("Encrypted and Decrypted values for RSA, must be exactly same", ORIGINAL_TEXT.equals(decryptedText));
+
+    }
+}
